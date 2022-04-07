ba8aeb2f3748d676c156df896206f04ce2df8fe8
sheetalskumar <sheetal.s.kumar@outlook.com>
2022-04-07 12:01:41 +1200

    Commit changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,38 @@
 import React from "react";
-
 import {
   Route,
   NavLink,
-  BrowserRouter as Router
+  BrowserRouter,
+  Routes
 } from "react-router-dom";
-
+import "./index.css";
 
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
+import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
 
-const App = () =>  {
-    return (
-        <Router>
-        <div>
-          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-            <ul className="header">
-            <li><NavLink exact to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
-            </ul>
-          <div className="content">
-          <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
-            <Route  path="/SubmitArticle" component={SubmitArticle}/>
-          </div>
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+        <ul className="header">
+          <li><NavLink exact="true" to="/">Home</NavLink></li>
+          <li><NavLink to="/SEPractice">Select the Practice</NavLink></li>
+          <li><NavLink to="/SubmitArticle">Submit an Article</NavLink></li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route exact="true" path="/" element={<Home />} />
+            <Route path="/SEPractice" element={<SEPractice />} />
+            <Route path="/SubmitArticle" element={<SubmitArticle />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </div>
-        </Router>
-    );
+      </div>
+    </BrowserRouter>
+  );
 }
- 
+
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./App";
- 
-ReactDOM.render(
-  <App/>, 
-  document.getElementById("root")
-);
+import App from "./App";
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
